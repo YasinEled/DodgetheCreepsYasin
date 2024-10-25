43f2e7b81fcb6c569922f8493576879b3477a113
YasinEled <yasinelede@gmail.com>
2024-10-25 15:21:12 +0200

    Assignació de tecles

--- Dodge The Creeps/Player.gd
+++ Dodge The Creeps/Player.gd
@@ -2,3 +2,6 @@ extends Area2D
 
 export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
 var screen_size # Mida de la finestra de joc.
+
+func _ready(): # es crida quan un node entra a l'arbre d'escena.
+	screen_size = get_viewport_rect().size 
